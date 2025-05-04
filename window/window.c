e307ab3a3e64f05c2fed9728001aba9ec7518f26
maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-05-04 22:55:48 +0200

    encapsulating window

---
+++ window/window.c
@@ -0,0 +1,5 @@
+//
+// Created by maxence on 04/05/25.
+//
+
+#include "window.h"
---
+++ window/window.h
@@ -0,0 +1,8 @@
+//
+// Created by maxence on 04/05/25.
+//
+
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#endif //WINDOW_H
