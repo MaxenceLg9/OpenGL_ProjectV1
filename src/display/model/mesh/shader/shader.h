e3233657f5564226f7d8be22d7066ae1791349c0
Lagourgue Maxence <maxence.lagourgue@free.fr>
2025-05-11 00:24:51 +0200

    porting shader to c++ class. Same logic remaining, just using C++ structures.

---
+++ src/display/model/mesh/shader/shader.h
@@ -0,0 +1,35 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include "GLAD/glad.h" // include glad to get all the required OpenGL headers
+
+class Shader{
+public:
+    Shader(const char *vertexPath, const char *fragmentPath);
+
+// use/activate the shader
+    void use() const;
+
+    void freeShader() const;
+
+    void setInt(const char *name, int value) const;
+
+    void setFloat(const char *name, float value) const;
+
+    void setVec2(const char *name, float v1, float v2) const;
+
+    void setVec4(const char *name, float v1, float v2, float v3, float v4) const;
+
+    void setMatrix4fv(const char *name, const float *matrix) const;
+
+    static void readFile(char **buffer, const char *filename);
+
+    static void compileShader(unsigned int *shader, const char **code, const int type, const char *typeName) ;
+
+    unsigned int id;
+};
+
+
+
+
+#endif
