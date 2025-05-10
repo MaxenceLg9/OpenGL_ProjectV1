e0398feabf01ef05a47d10d77100e5eeffa42d33
Lagourgue Maxence <maxence.lagourgue@free.fr>
2025-05-11 00:20:33 +0200

    class to implement all the additional math functions and grouping the #include headers

--- math/math.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by maxence on 05/05/25.
-//
-
-#ifndef MATH_H
-#define MATH_H
-#include <cglm/ivec3.h>
-
-#endif //MATH_H
---
+++ src/math/math.h
@@ -0,0 +1,13 @@
+//
+// Created by maxence on 05/05/25.
+//
+
+#ifndef MATH_H
+#define MATH_H
+#include "glm.hpp"
+#include "ext/matrix_transform.hpp"
+#include "ext/matrix_clip_space.hpp"
+#include "gtc/type_ptr.hpp"
+
+
+#endif //MATH_H
