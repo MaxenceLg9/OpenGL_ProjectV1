de515d2f67d4101c68e201ef3edbf1dacd1e3be5
Lagourgue Maxence <maxence.lagourgue@free.fr>
2025-05-11 22:14:12 +0200

    adding cursor lib files to build

--- src/display/window/cursor/cursor.h
+++ src/display/window/cursor/cursor.h
@@ -2,8 +2,8 @@
 // Created by Sinis on 08/05/2025.
 //
 
-#ifndef OPENGL_PROJECTV1_CURSOR_H
-#define OPENGL_PROJECTV1_CURSOR_H
+#ifndef CURSOR_H
+#define CURSOR_H
 
 #include "../../model/mesh/shader/shader.h"
 #include "../../model/mesh/mesh.h"
@@ -17,4 +17,4 @@ private:
     Mesh mesh;
 };
 
-#endif //OPENGL_PROJECTV1_CURSOR_H
+#endif //CURSOR_H
