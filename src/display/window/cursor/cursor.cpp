7e4f53f16c0af312fdabc576b12fca46956bced0
Lagourgue Maxence <maxence.lagourgue@free.fr>
2025-05-11 22:08:09 +0200

    small class using shader and mesh instances to draw a cursor on the screen. Not working

--- src/display/window/cursor/cursor.cpp
+++ src/display/window/cursor/cursor.cpp
@@ -30,7 +30,42 @@ unsigned int indices[] = {
         4, 5, 7, // fourth triangle
 };
 
-void drawCursor(){
-    static SHADER cursorShader;
-    getShader(&cursorShader, "shaders/cursor/vertex.ls", "shaders/cursor/fragment.ls");
+
+
+std::vector<TEXTURE> texturesArray;
+
+std::vector<VERTEX> verticesFromArray(){
+    std::vector<VERTEX> verticesArray;
+    for(int i = 0; i < sizeof(vertices)/sizeof(vertices[0]); i += 3) {
+        VERTEX vertex;
+        vertex.Position[0] = vertices[i];
+        vertex.Position[1] = vertices[i + 1];
+        vertex.Position[2] = vertices[i + 2];
+        vertex.TexCoords[0] = 0.0f;
+        vertex.TexCoords[1] = 0.0f;
+        vertex.Normal[0] = 1.0f;
+        vertex.Normal[1] = 1.0f;
+        vertex.Normal[2] = 1.0f;
+        verticesArray.push_back(vertex);
+    }
+    return verticesArray;
+}
+
+std::vector<unsigned int> indicesFromArray(){
+    std::vector<unsigned int> indicesArray;
+    for(unsigned int indice : indices) {
+        indicesArray.push_back(indice);
+    }
+    return indicesArray;
+}
+
+
+Cursor::Cursor() : shader("assets/shaders/cursor/vertex.ls", "assets/shaders/cursor/fragment.ls"),
+                   mesh(verticesFromArray(),indicesFromArray(),texturesArray)
+                   {
+}
+
+void Cursor::drawCursor(){
+    glDepthFunc(GL_ALWAYS); // Always pass the depth test (same effect as glDisable(GL_DEPTH_TEST))
+    mesh.draw(shader);
 }
--- src/display/window/cursor/cursor.h
+++ src/display/window/cursor/cursor.h
@@ -5,4 +5,16 @@
 #ifndef OPENGL_PROJECTV1_CURSOR_H
 #define OPENGL_PROJECTV1_CURSOR_H
 
+#include "../../model/mesh/shader/shader.h"
+#include "../../model/mesh/mesh.h"
+
+class Cursor {
+public:
+    void drawCursor();
+    Cursor();
+private:
+    Shader shader;
+    Mesh mesh;
+};
+
 #endif //OPENGL_PROJECTV1_CURSOR_H
