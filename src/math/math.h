b73f09fc9ca69dd8a83ee276d6cc97fd9038edca
Lagourgue Maxence <maxence.lagourgue@free.fr>
2025-08-11 23:37:56 +0200

    alpha function that is used to generate terrain

--- src/math/math.h
+++ src/math/math.h
@@ -18,4 +18,20 @@ struct IVec3Compare {
     }
 };
 
+namespace Utils {
+
+    inline float alpha(float t, int o) {
+        if (o == 0) {
+            return 1.0;
+        }
+        double frequency = 1.0;
+        double H = 0.75;
+        for (int i = 0; i < o; i++) {
+            frequency *= 2.0;
+        }
+        return t * pow(frequency, -H);
+    }
+
+}
+
 #endif //MATH_H
