326d77f7c68b078d72258b0ed274e29421812b97
Maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2026-05-13 13:15:06 +0200

    rebuilding the network stack on the server to provide high level API, and handle better the player update, etc...

--- src/server/network/client_connection.rs
+++ src/server/network/client_connection.rs
@@ -1,63 +1,62 @@
 use std::collections::HashSet;
 use std::io::{Error, ErrorKind};
 use std::net::SocketAddr;
-use std::ops::Deref;
+use std::ops::{Add, Deref, Mul};
 use std::sync::{Arc, RwLock};
 use std::time::{Duration, Instant};
 use tokio::sync::mpsc;
-use shared::common::network::client::packet::ClientPacket;
+use shared::common::network::default_packet::ClientPacket;
 use shared::common::network::server::tick_player::GetPlayerPacket;
 use shared::common::world::pos::chunkpos::ChunkPos;
 use shared::{print_base};
 use shared::common::account::puid::PUID;
 use shared::common::network::client::player_packet::UpdatePlayerPacket;
 use shared::common::network::server::connection_packet::ConnectionPacket;
-use shared::common::network::server::packet::ServerPacket;
+use shared::common::network::default_packet::ServerPacket;
+use shared::common::network::packet_type::ServerPacketType::Chunk;
 use shared::common::network::server::quit_packet::QuitPacket;
+use crate::server::network::socket::ServerConnection;
+use crate::server::world_data::chunk::chunk_map::ServerChunkMap;
 use crate::server::world_data::data::ServerWorldData;
 use crate::server::world_data::player::player::ServerPlayer;
 
 pub struct ClientConnection {
     server_world_data: Arc<ServerWorldData>,
-    addr : SocketAddr,
     chunks_loaded : HashSet<ChunkPos>,
     player : Arc<RwLock<ServerPlayer>>,
-    packet_receiver : mpsc::Receiver<ClientPacket>,
     chunks_pos: ChunkPos,
-    socket : Arc<tokio::net::UdpSocket>,
+    socket : ServerConnection,
     prx : tokio::sync::mpsc::Receiver<ServerPacket>,
     view_distance : u8,
-    chunks_to_load: HashSet<ChunkPos>,
     puid : PUID,
     ping : Instant,
     ping_id : Option<u16>
 }
 
 impl ClientConnection {
-    pub async fn start(addr : SocketAddr, packet : ClientPacket, packet_receiver: mpsc::Receiver<ClientPacket>, socket : Arc<tokio::net::UdpSocket>, server_world_data: Arc<ServerWorldData>) {
-        let (psx, prx) = mpsc::channel::<ServerPacket>(10000);
+    pub async fn start(packet : ClientPacket, socket : ServerConnection, server_world_data: Arc<ServerWorldData>) {
+        let (psx, prx) = mpsc::channel::<ServerPacket>(100000);
         let ClientPacket::Login(con_packet) = packet else {
-            print_base!("Connection {}: Wrong Packet, returning", addr);
+            print_base!("Connection {}: Wrong Packet, returning", socket.get_addr());
             return;
         };
 
-        let Ok((pos, player)) = server_world_data.connect_player(con_packet.get_puid().clone(), psx).inspect_err(|e| print_base!("Connection {}: Got Error {}, returning", addr, e)) else {
+        let Ok((pos, player)) = server_world_data
+            .connect_player(con_packet.get_puid().clone(), psx)
+            .inspect_err(|e| print_base!("Connection {}: Got Error {}, returning", socket.get_addr(), e)) else {
             return;
         };
-        socket.send_to(ServerPacket::Connect(ConnectionPacket::new(pos)).encode().as_raw_slice(),addr).await.unwrap();
+        socket.send(ServerPacket::Connect(ConnectionPacket::new(pos))).await.unwrap();
 
 
 
         let mut client_connection = Self {
             chunks_loaded: HashSet::new(),
             chunks_pos: pos.get_chunk_pos(),
-            packet_receiver,
             socket,
             server_world_data,
-            chunks_to_load : HashSet::new(),
             player,
             prx,
-            addr,
             ping: Instant::now(),
             ping_id: Some(0),
             view_distance: 8,
@@ -74,15 +73,14 @@ impl ClientConnection {
         let mut instant = Instant::now();
         let mut id = 0;
         self.generate_chunks();
-        self.get_chunks_to_load();
-        self.send_chunks();
+        Self::load_chunks(self.server_world_data.get_chunk_map().clone(), self.player.clone(), self.chunks_pos,self.chunks_pos,0,10);
 
         loop {
             tokio::select! {
                 // waiting till the socket receives data or timeout limit is reached
-                packet = self.packet_receiver.recv() => {
+                packet = self.socket.recv() => {
                     if let Err(e) = self.receive(&packet) {
-                        print_base!("Exiting {} due to error {}", self.addr, e);
+                        print_base!("Exiting {} due to error {}", self.socket.get_addr(), e);
                         break;
                     } else {
                         instant = Instant::now();
@@ -92,17 +90,14 @@ impl ClientConnection {
                 // if a packet is scheduled to be sent
                 Some(packet_data) = self.prx.recv() => {
                     // ChunkPacket::from_bits(packet_data[1..].view_bits::<Lsb0>().to_bitvec());
-                    self.socket.send_to(packet_data.encode().as_raw_slice(), self.addr).await.unwrap();
-                    if let ServerPacket::Chunk(p) = packet_data {
-                        self.chunks_to_load.remove(&p.get_chunk_pos());
-                    };
+                    self.socket.send(packet_data).await.unwrap();
                 }
 
                 // querying the player information at a constant interval
                 _ = heartbeat_interval.tick() => {
                     // checking if the timeout of 5 isn't elapsed
                     if instant.elapsed().gt(&Duration::from_secs(5)) {
-                        print_base!("Exiting {} due to time elapsed", self.addr);
+                        print_base!("Exiting {} due to time elapsed", self.socket.get_addr());
                         break;
                     }
                     if self.ping_id.is_none() {
@@ -112,11 +107,11 @@ impl ClientConnection {
                     // sending the packet to query the information of the player
                     let packet = ServerPacket::GetPlayer(GetPlayerPacket::new(id));
                     id += 1;
-                    self.socket.send_to(packet.encode().as_raw_slice(),self.addr).await.unwrap();
+                    self.socket.send(packet).await.unwrap();
                 }
             }
         }
-        self.socket.send_to(ServerPacket::Quit(QuitPacket::new()).encode().as_raw_slice(), self.addr).await.expect("");
+        self.socket.send(ServerPacket::Quit(QuitPacket::new())).await.expect("");
         self.server_world_data.disconnect_player(&self.puid);
     }
 
@@ -124,25 +119,14 @@ impl ClientConnection {
         let mut array: [ChunkPos; 20*20*20] = [ChunkPos::from_i32(0, 0, 0);20*20*20];
         let mut i = 0;
         for x in -10..10 {
-            for y in -10..10 {
+            for y in 0..8 {
                 for z in -10..10 {
-                    array[i] = self.chunks_pos + ChunkPos::from_i32(x, y, z);
+                    array[i] = self.chunks_pos.flattened() + ChunkPos::from_i32(x, y, z);
                     i += 1;
                 }
             }
         }
         self.server_world_data.get_generator().write().unwrap().schedule_chunks(array);
-    }
-
-    fn send_chunks(&mut self) {
-        let start = Instant::now();
-        let arc_chunk_map = self.server_world_data.get_chunk_map().clone();
-        let mut chunk_map = arc_chunk_map.write().unwrap();
-        for chunk_pos in self.chunks_to_load.drain() {
-            self.chunks_loaded.insert(chunk_pos);
-            chunk_map.ask_for_chunks(chunk_pos,self.player.clone());
-        }
-        // print_base!("Sent chunks in {}ms", Instant::now().duration_since(start).as_millis());
     }
 
     fn receive(&mut self, frame: &Option<ClientPacket>) -> Result<(), Error> {
@@ -156,14 +140,15 @@ impl ClientConnection {
         }
     }
 
-    fn get_chunks_to_load(&mut self) {
-        for i in 0..(self.view_distance as i32 * 2).pow(3) {
-            let pos = ChunkPos::from_single_value(i, self.view_distance as i32 * 2) + self.chunks_pos;
-            if !self.chunks_loaded.contains(&pos) {
-            self.chunks_to_load.insert(pos);
-            }
+    fn load_chunks(arc_chunk_map :  Arc<RwLock<ServerChunkMap>>, server_player: Arc<RwLock<ServerPlayer>>, last_pos : ChunkPos, new_pos : ChunkPos, old_view_distance : i32, new_view_distance : i32) {
+        let mut chunk_map = arc_chunk_map.write().unwrap();
+        let flattened_pos = new_pos.flattened();
+        let (v1, v2 ) = ServerChunkMap::compute_chunk_diff(last_pos, new_pos, old_view_distance, new_view_distance);
+        print_base!("Chunks to load {}", v1.len());
+        for pos in v1 {
+            chunk_map.ask_for_chunks(pos, server_player.clone());
         }
-        print_base!("len of chunks to load : {}",self.chunks_to_load.len());
+
     }
 
     fn handle_packet(&mut self, packet : &ClientPacket) -> Result<(), Error> {
@@ -172,7 +157,7 @@ impl ClientConnection {
             ClientPacket::UpdatePlayer(p) => {
                 self.update_player(p);
                 if Some(p.get_id()) == self.ping_id {
-                    print_base!("Ping of : {}ms",Instant::now().duration_since(self.ping).as_millis());
+                    // print_base!("Ping of : {}ms",Instant::now().duration_since(self.ping).as_millis());
                     self.ping_id = None;
                 }
                 Ok(())
@@ -185,16 +170,24 @@ impl ClientConnection {
     /// schedule to load chunks if needed
     fn update_player(&mut self, packet : &UpdatePlayerPacket) {
         self.player.write().unwrap().set_pos(packet.get_pos());
-        if packet.get_pos().get_chunk_pos() != self.chunks_pos {
+
+        let moved = packet.get_pos().get_chunk_pos() != self.chunks_pos;
+        let viewed = self.view_distance != packet.get_view_distance();
+
+        let last_pos = self.chunks_pos;
+
+        if viewed {
+            self.view_distance = packet.get_view_distance();
+        }
+
+        if moved {
             self.chunks_pos = packet.get_pos().get_chunk_pos();
             self.generate_chunks();
-            self.get_chunks_to_load();
-            self.send_chunks();
             print_base!("New ChunkPos : {}", self.chunks_pos.deref());
         };
-        if self.view_distance != packet.get_view_distance() {
-            self.view_distance = packet.get_view_distance();
-            self.get_chunks_to_load();
+
+        if moved || viewed {
+            Self::load_chunks(self.server_world_data.get_chunk_map().clone(), self.player.clone(),last_pos, self.chunks_pos, self.view_distance as i32, self.view_distance as i32);
         }
     }
 }
--- src/server/network/socket.rs
+++ src/server/network/socket.rs
@@ -1,91 +1,70 @@
-use std::collections::HashMap;
 use std::io::{Error, ErrorKind};
-use crate::server::world_data::data::ServerWorldData;
-use shared::print_base;
 use std::net::{SocketAddr, SocketAddrV6};
-use std::sync::{Arc, RwLock};
+use std::sync::Arc;
 use tokio::net::UdpSocket;
-use shared::common::network::client::packet::ClientPacket;
-use shared::common::network::packet_type::{ClientPacketType, ServerPacketType};
-use shared::common::network::packet_type::ConnectionState::Quit;
-use shared::common::network::server::packet::ServerPacket;
-use shared::common::world::chunk::chunkmap::ChunkMap;
-use crate::server::network::client_connection::ClientConnection;
+use tokio::sync::mpsc;
+use shared::common::network::default_packet::{ClientPacket, ServerPacket};
 
 pub struct ServerSocket {
+    udp_socket: Arc<UdpSocket>,
+    buffer : [u8; 1024],
+}
+
+pub struct ServerConnection {
+    udp_socket: Arc<UdpSocket>,
+    socket_addr : SocketAddr,
+    packet_receiver: mpsc::Receiver<ClientPacket>,
 }
 
 impl ServerSocket {
 
-    pub fn new() -> Self {
-        Self {
-        }
+    pub async fn new(socket_addr_v6: SocketAddrV6) -> Result<Self,Error> {
+        let socket = UdpSocket::bind(socket_addr_v6).await?;
+
+        Ok(Self {
+            udp_socket : Arc::new(socket),
+            buffer : [0;1024]
+        })
     }
-
-    pub fn listen(&self, server_world_data: Arc<ServerWorldData>, socket_addr_v6: SocketAddrV6, ) {
-        std::thread::Builder::new()
-            .name("network_thread".to_string())
-            .spawn(move || {
-                // Create a single-threaded executor for this specific thread
-                Self::thread_connections(server_world_data, socket_addr_v6);
-            })
-            .unwrap();
-    }
-
-    fn thread_connections(server_world_data: Arc<ServerWorldData>, socket_addr_v6: SocketAddrV6, ) {
-        let rt = tokio::runtime::Builder::new_current_thread()
-            .enable_all()
-            .build()
-            .unwrap();
-
-        rt.block_on(async {
-            let socket = Arc::new(UdpSocket::bind(socket_addr_v6).await.unwrap());
-            let mut map = HashMap::new();
-            let mut buff: [u8; 1024]  = [0;1024];
-            print_base!("ServerSocket binding to {:?}", socket_addr_v6);
-            loop {
-                // waiting till the socket receives data or timeout limit is reached
-                let result = socket.recv_from(&mut buff).await;
-                let (packet, addr) = match Self::receive(result,buff) {
-                    Err(e) => {
-                        print_base!("Got error when receiving packet {}",e.to_string());
-                        continue;
-                    }
-                    Ok(p) => p,
-                };
-                // print_base!("Got packet {}", packet);
-                if packet.get_packet_type() == ClientPacketType::Login {
-                    print_base!("Got Connection from {}", addr);
-                    let (sender, receiver) = tokio::sync::mpsc::channel(1000);
-                    map.insert(addr,sender);
-                    let world_ref = server_world_data.clone();
-                    let clone = socket.clone();
-                    tokio::spawn(async move {
-                        ClientConnection::start(addr, packet, receiver, clone, world_ref).await;
-                        print_base!("Connection {} finished", addr);
-                    });
-                } else {
-                    if let Err(e) = map.get(&addr).unwrap().send(packet).await {
-                        print_base!("Error when sending packet : {}",e);
-                    }
-                }
-
-            }
-        });
-    }
-
-    fn receive(frame : Result<(usize, SocketAddr), Error>, buff : [u8; 1024]) -> Result<(ClientPacket, SocketAddr), Error> {
+    pub async fn receive(&mut self) -> Result<(ClientPacket, SocketAddr), Error> {
+        let frame = self.udp_socket.recv_from(&mut self.buffer).await;
         match frame {
             Ok(result) => {
                 /// trying to parse the raw bytes into a struct
-                match ClientPacket::decode(&buff[0..result.0].to_vec()){
+                match ClientPacket::decode(&self.buffer[0..result.0].to_vec()){
                     Some(p) => Ok((p,result.1)),
-                    None => return Err(Error::new(ErrorKind::StaleNetworkFileHandle,"Cannot parse the packet, invalid bytes")),
+                    None => Err(Error::new(ErrorKind::StaleNetworkFileHandle,"Cannot parse the packet, invalid bytes")),
                 }
             }
             Err(e) => {
                 Err(e)
             }
         }
+    }
+
+    pub fn get_child(&self, socket_addr : SocketAddr, packet_receiver: mpsc::Receiver<ClientPacket>) -> ServerConnection {
+        ServerConnection::new(self.udp_socket.clone(), socket_addr, packet_receiver)
+    }
+}
+
+impl ServerConnection {
+    pub fn new(udp_socket: Arc<UdpSocket>, socket_addr: SocketAddr, packet_receiver: mpsc::Receiver<ClientPacket>) -> ServerConnection {
+        Self {
+            udp_socket,
+            socket_addr,
+            packet_receiver
+        }
+    }
+
+    pub fn get_addr(&self) -> SocketAddr {
+        self.socket_addr
+    }
+
+    pub async fn send(&self, server_packet: ServerPacket) -> std::io::Result<usize> {
+        self.udp_socket.send_to(server_packet.encode().as_raw_slice(),self.socket_addr).await
+    }
+    
+    pub async fn recv(&mut self) -> Option<ClientPacket> {
+        self.packet_receiver.recv().await
     }
 }
